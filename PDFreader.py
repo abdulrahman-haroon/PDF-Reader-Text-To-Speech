e0b84f14dc0a85b471912fa9cbc430188d6618bf
abdulrahman-haroon <abdulrahman.haroonr@gmail.com>
2020-07-16 19:37:28 +0500

    Finalized

--- PDFreader.py
+++ PDFreader.py
@@ -1,1 +1,14 @@
-print("This is a PDF Reader Text To Speech!")
+import pyttsx3
+import PyPDF2
+
+pdfFile=open('DoingDataScience.pdf','rb')
+pdfReader=PyPDF2.PdfFileReader(pdfFile)
+pages = pdfReader.numPages #This is to know the pages in PDF file.
+#print(pages)
+
+reader=pyttsx3.init()
+for numOfPage in range(16,pages):
+    page=pdfReader.getPage(numOfPage)
+    text=page.extractText()
+    reader.say(text)
+    reader.runAndWait()
